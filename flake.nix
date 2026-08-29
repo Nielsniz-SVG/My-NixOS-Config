a6edb88c5b3bcd7eb5cb3a5530eb3909bf49bd32
Nielsniz-SVG <nielsboulengernincq@gmail.com>
2026-08-29 18:35:34 +0200

    Changes to be committed: 	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -5,21 +5,23 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
-    
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     silentSDDM = {
       url = "github:uiriansan/SilentSDDM";
       inputs.nixpkgs.follows = "nixpkgs";
-   };
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
+      specialArgs = {
+        inherit inputs;
+        hardwareConfiguration = /etc/nixos-local/hardware-configuration.nix;
+      };
+
       modules = [
         ./configuration.nix
         inputs.home-manager.nixosModules.default
